63b4ea5c38e04a66c5473a0755331ec906f7b219
PeriodWisePwa <gblissstore2@gmail.com>
2026-01-26 16:36:21 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,25 +1,34 @@
- const CACHE_NAME = "periodwise-cache-v2";
+const CACHE_NAME = "periodwise-cache-v3";
 
 const urlsToCache = [
-  "/",
-  "/index.html",
-  "/manifest.json"
+  "/PeriodWise-Pwa/",
+  "/PeriodWise-Pwa/index.html",
+  "/PeriodWise-Pwa/manifest.json"
 ];
+
 self.addEventListener("install", event => {
-  // Cache files for offline use
   event.waitUntil(
-    caches.open("periodwise-cache").then(cache => {
-      return cache.addAll([
-        "/PeriodWise-Pwa/",
-        "/PeriodWise-Pwa/index.html",
-        "/PeriodWise-Pwa/manifest.json"
-      ]);
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
     })
   );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      );
+    })
+  );
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
-  // Serve cached content if offline
   event.respondWith(
     caches.match(event.request).then(response => {
       return response || fetch(event.request);
