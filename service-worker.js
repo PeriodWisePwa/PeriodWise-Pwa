a7f943a2e1fd3f685f5a8f714d66bbc4366615d8
PeriodWisePwa <gblissstore2@gmail.com>
2026-02-12 17:26:57 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "periodwise-cache-v26";  // Update cache version number
+const CACHE_NAME = "periodwise-cache-v27";  // Update cache version number
 
 // List of files to cache (including updates.json)
 const urlsToCache = [
