19b18d2a7758337fd787c044118d4538e5b02500
PeriodWisePwa <gblissstore2@gmail.com>
2026-02-21 10:54:41 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@
 // PeriodWise Service Worker
 // ==============================
 
-const CACHE_NAME ='periodwise-v22'
+const CACHE_NAME ='periodwise-v23'
 const urlsToCache = [
   './',
   './index.html',
